58f73b78f924a4a0cf3f2dc80a4309038402dd58
Ruben Martinez Jr <rubencodes@users.noreply.github.com>
2020-05-24 10:29:10 -0400

    Update to avoid weird refreshing state.

--- components/PhotoGrid.js
+++ components/PhotoGrid.js
@@ -33,7 +33,7 @@ const PhotoGrid = ({ baseUrl, images }) => {
 				return;
 			}
 		}
-	}, [images]);
+	}, [images, selectedImageIndex]);
 
 	return (
 		<>
--- components/useQueryStringState.js
+++ components/useQueryStringState.js
@@ -2,30 +2,57 @@ import { useRouter } from "next/router";
 
 const DO_NOTHING = (i) => i;
 
+/*
+	Nextjs won't automatically remove queryparams
+	that are actually route params, so this does.
+*/
+const removeKeysInUrl = (object, url) => {
+	const copy = { ...object };
+	Object.keys(object).forEach((key) => {
+		if (url.includes(`[${key}]`)) {
+			delete copy[key];
+		}
+	});
+
+	return copy;
+};
+
 function useQueryStringState(queryParamName, processValue = DO_NOTHING) {
-	const router = useRouter();
-	const queryParams = router.query;
-	const state = queryParams[queryParamName];
-	const setState = (val) => {
+	const { query: queryParams, pathname, push } = useRouter();
+	const { [queryParamName]: state, ...otherQueryParams } = queryParams;
+	const setState = (newValue) => {
 		// Do nothing if value hasn't changed.
-		if (val === processValue(queryParams[queryParamName])) {
+		if (newValue === processValue(state)) {
 			return;
 		}
 
 		// Remove it if value is nullish.
-		if (val === null || val === undefined) {
-			const { [queryParamName]: omit, ...rest } = queryParams;
-			router.push({
+		if (newValue === null || newValue === undefined) {
+			push({
+				pathname,
+				query: otherQueryParams,
+			}, {
 				pathname: location.pathname,
-				query: rest,
+				query: removeKeysInUrl(otherQueryParams, pathname),
+			}, {
+				shallow: true,
 			});
 			return;
 		}
 
 		// Push to the new value.
-		router.push({
+		const nextQueryParams = {
+			...otherQueryParams,
+			[queryParamName]: newValue
+		};
+		push({
+			pathname,
+			query: nextQueryParams,
+		}, {
 			pathname: location.pathname,
-			query: { ...queryParams, [queryParamName]: val },
+			query: removeKeysInUrl(nextQueryParams, pathname),
+		}, {
+			shallow: true,
 		});
 	}
 
