14a9471f4283c379ebe24944f3a7763e4db5d5ba
Ruben Martinez Jr <rubencodes@users.noreply.github.com>
2020-05-26 15:56:14 -0400

    Add website og:type.

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ const AppContainer = ({ Component, pageProps }) => (
 		<Head>
 			<title>{state.metaTitle}</title>
 			<meta charset="utf-8" />
+			<meta property="og:type" content="website" />
 			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 			<meta name="twitter:card" content="summary_large_image" />
 			<meta name="twitter:site" content={state.metaTwitter} />
