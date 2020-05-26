b2b88f875544fd28593cf528ecf63c70e65ae8e9
Ruben Martinez Jr <rubencodes@users.noreply.github.com>
2020-05-26 16:50:21 -0400

    Fix for license crap.

--- components/ImageLicenseData.js
+++ components/ImageLicenseData.js
@@ -3,15 +3,19 @@ import Head from "next/head";
 
 const ImageLicenseData = ({ imageUrl }) => (
 	<Head>
-		<script key={imageUrl} type="application/ld+json">
-			{JSON.stringify({
-				"@context": "https://schema.org/",
-				"@type": "ImageObject",
-				"url": imageUrl,
-				"license": "https://creativecommons.org/licenses/by-nc/4.0/",
-				"acquireLicensePage": "https://ruben.codes"
-			})}
-		</script>
+		<script
+			key={imageUrl}
+			type="application/ld+json"
+			dangerouslySetInnerHTML={{
+				__html: JSON.stringify({
+					"@context": "https://schema.org/",
+					"@type": "ImageObject",
+					"url": imageUrl,
+					"license": "https://creativecommons.org/licenses/by-nc/4.0/",
+					"acquireLicensePage": "https://ruben.codes"
+				})
+			}}
+		/>
 	</Head>
 );
 
