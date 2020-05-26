0823437e2c30db07cef95bb44c484d601e5d7595
Ruben Martinez Jr <rubencodes@users.noreply.github.com>
2020-05-26 13:59:30 -0400

    Fix an oopsie.

--- pages/_app.js
+++ pages/_app.js
@@ -8,14 +8,14 @@ const AppContainer = (props) => (
 		<Head>
 			<title>Ruben Martinez Jr - Artist, Developer, Technologist</title>
 			<meta charset="utf-8" />
-			<link rel="icon" type="image/png" href="https://ruben.codes/static/favicon.png" />
 			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 			<meta name="theme-color" content="#000000" />
+			<meta name="twitter:site" content="@rubencodes" />
+			<meta name="twitter:card" content="summary_large_image" />
+			<link rel="icon" type="image/png" href="https://ruben.codes/static/favicon.png" />
 			<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Serif:400,700" rel="stylesheet" />
 			<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:300,700" rel="stylesheet" />
 			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css" />
-			<meta name="twitter:site" content="@rubencodes" />
-			<meta name="twitter:card" content="summary_large_image" />
 		</Head>
 		<App {...props} />
 	</>
