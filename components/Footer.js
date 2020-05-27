d39a6995d4d2b05d87082aa6bcdad85715caca56
Ruben Martinez Jr <rubencodes@users.noreply.github.com>
2020-05-26 22:25:40 -0400

    ©

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ import styles from "./Footer.module.css";
 const Footer = () => (
 	<footer className={styles.footer}>
 		<p className={styles.footerInner}>
-			Copyright {(new Date()).getFullYear()} Ruben Martinez Jr.
+			Copyright © {(new Date()).getFullYear()} Ruben Martinez Jr.
 		</p>
 	</footer>
 );
