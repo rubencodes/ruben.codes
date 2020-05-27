9ebcf049c08849949e6df673263e1d51f0e65bda
Ruben Martinez Jr <rubencodes@users.noreply.github.com>
2020-05-27 12:14:16 -0400

    Update description.

--- utilities/photography.js
+++ utilities/photography.js
@@ -1,6 +1,6 @@
 export default {
 	metaTitle: "Ruben Martinez Jr. - Photography",
-	metaDescription: "Explore artistic boudoir portraits, urban street portraits, and natural landscape photo galleries from New York City photographer Ruben Martinez Jr. Ruben is shooting with a Sony A7rIV camera.",
+	metaDescription: "Explore artistic boudoir portraits, urban street portraits, and natural landscape photo galleries from New York City based photographer Ruben Martinez Jr. Ruben is shooting on the street and in the studio with a Sony A7rIV mirrorless camera.",
 	metaUrl: "https://ruben.codes/photography",
 	metaImage: "https://ruben.codes/static/photography/landscapes/46.jpg",
 
