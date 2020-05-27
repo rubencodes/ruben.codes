2082a78b57e0ff8c254d17bbd5e1a2b8844d2809
Ruben Martinez Jr <rubencodes@users.noreply.github.com>
2020-05-26 22:43:51 -0400

    Add documentation.

--- components/useLazyEffect.js
+++ components/useLazyEffect.js
@@ -3,15 +3,18 @@ import { useEffect, useState } from "react";
 function useLazyEffect(effectFunction, deps) {
 	const [targetElement, setTargetElement] = useState(null);
 	useEffect(() => {
+		// No target element or effect function yet, just give up.
 		if (!targetElement || typeof effectFunction !== "function") {
 			return;
 		}
 
+		// No IntersectionObserver support, just give up and call the function.
 		if (!("IntersectionObserver" in window)) {
 			effectFunction(targetElement);
 			return;
 		}
 
+		// Setup IntersectionObserver to watch for intersections.
 		const targetElementObserver = new IntersectionObserver(function (entries) {
 			entries.forEach(function (entry) {
 				if (!entry.isIntersecting) {
