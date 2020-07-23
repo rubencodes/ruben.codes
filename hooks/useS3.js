e9acb1fe02128218e88c56ad4fa4d180285c18e3
Ruben Martinez Jr <rubencodes@users.noreply.github.com>
2020-07-22 21:40:50 -0400

    Fix credentials issue.

--- hooks/useS3.js
+++ hooks/useS3.js
@@ -3,7 +3,7 @@ import { useMemo } from "react";
 
 const AWS_S3_SDK = "https://sdk.amazonaws.com/js/aws-sdk-2.717.0.min.js";
 
-function useS3({ bucketName, bucketRegion, IdentityPoolId, IdentityId }) {
+function useS3({ bucketName, bucketRegion, credentials }) {
 	const isReady = useScript(AWS_S3_SDK);
 	const s3 = useMemo(() => {
 		if (!isReady) return null;
@@ -11,9 +11,7 @@ function useS3({ bucketName, bucketRegion, IdentityPoolId, IdentityId }) {
 		// Authenticate credentials.
 		AWS.config.update({
 			region: bucketRegion,
-			credentials: {
-			}
-			// })
+			credentials,
 		});
 
 		return new AWS.S3({
