28fa7331bc567cc9f927c766c5c5000b75b5af92
Ruben Martinez Jr <rubencodes@users.noreply.github.com>
2020-07-25 19:35:12 -0400

    Cleanup

--- utilities/fetchConfig.js
+++ utilities/fetchConfig.js
@@ -1,7 +1,6 @@
 import fetch from "isomorphic-unfetch";
 
 const fetchConfig = async (url) => {
-  console.log(url);
   // Fetch data from external API
   const res = await fetch(url);
   return res.json();
