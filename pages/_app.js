f982ade5c988c9be6286db13ae934dd8eddbc1ea
Ruben Martinez Jr <rubencodes@users.noreply.github.com>
2020-10-20 23:05:41 -0400

    Use CSS preload.

--- pages/_app.js
+++ pages/_app.js
@@ -20,21 +20,47 @@ const AppContainer = ({ Component, pageProps }) => (
       <meta name="theme-color" content={state.metaThemeColor} />
       <link rel="icon" type="image/png" href={state.metaFavicon} />
       <link
+        rel="preload"
         href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:400"
-        rel="stylesheet"
+        as="style"
+        onLoad="this.onload=null;this.rel='stylesheet'"
       />
       <link
+        rel="preload"
         href="https://fonts.googleapis.com/css?family=IBM+Plex+Serif:400,700"
-        rel="stylesheet"
+        as="style"
+        onLoad="this.onload=null;this.rel='stylesheet'"
       />
       <link
+        rel="preload"
         href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:300,700"
-        rel="stylesheet"
+        as="style"
+        onLoad="this.onload=null;this.rel='stylesheet'"
       />
       <link
-        rel="stylesheet"
+        rel="preload"
         href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css"
+        as="style"
+        onLoad="this.onload=null;this.rel='stylesheet'"
       />
+      <noscript>
+        <link
+          href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:400"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=IBM+Plex+Serif:400,700"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:300,700"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css"
+        />
+      </noscript>
     </Head>
     <Component {...pageProps} />
   </>
