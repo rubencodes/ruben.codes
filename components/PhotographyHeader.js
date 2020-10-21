55e2e7fbd0c23f0ef11f475e23bf7e119831acb2
Ruben Martinez Jr <rubencodes@users.noreply.github.com>
2020-10-20 20:27:47 -0400

    Add About page.

--- components/PhotographyHeader.js
+++ components/PhotographyHeader.js
@@ -1,20 +1,35 @@
 import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 import RMJLogo from "./RMJLogo.svg";
 import InstagramLink from "./InstagramLink";
 import ContactLink from "./ContactLink";
 
-const PhotographyHeader = ({ subheader }) => (
-  <>
-    <h1 className="heading">
-      <img className="heading-image" src={RMJLogo} alt="RMJ Photography" />
-      <div className="heading-actions">
-        <InstagramLink />
-        <ContactLink />
-      </div>
-    </h1>
-    <b className="subheading">{subheader}</b>
-  </>
-);
+const GALLERIES_PATH = "/photography";
+const ABOUT_PAGE_PATH = "/photography/about";
+
+const PhotographyHeader = ({ subheader }) => {
+  const { pathname } = useRouter();
+  const isAboutPage = pathname === ABOUT_PAGE_PATH;
+
+  return (
+    <>
+      <h1 className="heading">
+        <img className="heading-image" src={RMJLogo} alt="RMJ Photography" />
+        <div className="heading-actions">
+          <InstagramLink />
+          <ContactLink />
+          {isAboutPage ? (
+            <Link href={GALLERIES_PATH}>View Galleries</Link>
+          ) : (
+            <Link href={ABOUT_PAGE_PATH}>About Me</Link>
+          )}
+        </div>
+      </h1>
+      <b className="subheading">{subheader}</b>
+    </>
+  );
+};
 
 export default PhotographyHeader;
---
+++ pages/photography/about.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Head from "next/head";
+import classnames from "classnames";
+
+import PhotographyHeader from "../../components/PhotographyHeader";
+import Footer from "../../components/Footer";
+import { state } from "../../utilities/constants";
+
+import styles from "./index.module.css";
+
+const About = () => (
+  <>
+    <Head>
+      <title>{state.photography.metaTitle}</title>
+      <meta
+        key="title"
+        property="og:title"
+        content={state.photography.metaTitle}
+      />
+      <meta
+        key="description"
+        property="og:description"
+        content={state.photography.metaDescription}
+      />
+      <meta key="url" property="og:url" content={state.photography.metaUrl} />
+      <meta
+        key="image"
+        property="og:image"
+        content={state.photography.metaImage}
+      />
+    </Head>
+    <main className={classnames("page", styles.photography)}>
+      <PhotographyHeader subheader="About" />
+      <p className="text">
+        My name is Ruben Martinez Jr. I'm a New York City based photographer and
+        software engineer. My artistic interests are mainly around capturing
+        human portraits, both in the studio and out in the real world. I most
+        often shoot with my Sony A7RIV mirrorless camera, and I prefer to shoot
+        with a macro lens to capture more intimately framed shots. Check out my
+        photo galleries via the link above, and feel free to contact me if you
+        see anything you like or if you'd be interested in collaborating on a
+        shoot.
+      </p>
+    </main>
+    <Footer />
+  </>
+);
+
+export default About;
